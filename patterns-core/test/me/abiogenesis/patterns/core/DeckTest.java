16dd5cc1958fd68dcc53d597ca0d63746230f74e
Evan Moritz <emoritzx@gmail.com>
2017-10-17 19:25:16 -0400

    Added tests

---
+++ patterns-core/test/me/abiogenesis/patterns/core/DeckTest.java
@@ -0,0 +1,19 @@
+package me.abiogenesis.patterns.core;
+
+import org.testng.annotations.Test;
+import java.util.List;
+import java.util.Stack;
+import static org.testng.Assert.*;
+
+public class DeckTest {
+    
+    @Test
+    public void testCreate() throws Exception {
+        Stack<Card> deck = Deck.create();
+        assertEquals(
+            deck.size(),
+            Deck.PROPERTIES.stream()
+                .mapToInt(List::size)
+                .reduce(1, (a, b) -> a * b));
+    }
+}
